2956b86fca6ea973303050cf5d1fe7e81471ab3c
Andrew Chap <andrew@andrewchap.com>
2019-12-22 08:52:16 -0700

    Updating helper functions to work correctly

--- helpers.sh
+++ helpers.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-function g(){
-    echo cd /home/amchap06/github/mortgagetvm/mortgagetvm
-    cd /home/amchap06/github/mortgagetvm/mortgagetvm
-}
+#function g(){
+#    echo cd /home/amchap06/github/mortgagetvm/mortgagetvm
+#    cd /home/amchap06/github/mortgagetvm/mortgagetvm
+#}
 function c(){
-    echo vi assets/3_mort.css
-    vi assets/3_mort.css
+    echo vi app/assets/3_stock.css
+    vi app/assets/3_stock.css
 }
 # helper function to vim app
 function v(){
-    main=$(s $1)
+    main=$(switchmain $1)
     echo vi $main
     vi $main
 }
 # helper function to test app
 function t(){
-    main=$(s $1)
-    $cp $main main.py
-    $echo $main main.py
+    main=$(switchmain $1)
+    cp $main app/main.py
+    echo cp $main app/main.py
     echo ./t.sh $main
     ./t.sh $main
 }
 #helper function to set which app we are working on
-function s(){
+function switchmain(){
     if [ -z "$1" ]
     then
         selection=$(cat current.txt)
@@ -31,9 +31,8 @@ function s(){
         selection=$1
         echo $selection > current.txt
     fi
-    main=main${selection}.py 
+    main="app/main${selection}.py" 
     echo $main
     #echo cp $main main.py
     #cp $main main.py
 }
-
